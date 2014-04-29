885324bce2c729389ad5d907c1948f60bb63c29a
Sam Davies <sdavies@shinobicontrols.com>
2014-04-29 16:45:09 +0100

    Added JSON parsing
    Now creates NSDate object rather than NSString

--- ReactiveWikiMonitor/SCWebSocketConnector.m
+++ ReactiveWikiMonitor/SCWebSocketConnector.m
@@ -56,8 +56,15 @@
 #pragma mark - SRWebSocketDelegate Methods
 - (void)webSocket:(SRWebSocket *)webSocket didReceiveMessage:(id)message
 {
+    NSError *error;
+    NSDictionary *deserialised = [self parseWikipediaUpdateMessage:message error:&error];
+    if(error) {
+        NSLog(@"Error parsing JSON String: %@", error);
+        return;
+    }
+    
     [self.scheduler schedule:^{
-        [self.messagesSubj sendNext:message];
+        [self.messagesSubj sendNext:deserialised];
     }];
 }
 
@@ -73,6 +80,34 @@
     [self.scheduler schedule:^{
         [self.messagesSubj sendCompleted];
     }];
+}
+
+#pragma mark - Utility Methods
+- (NSDictionary *)parseWikipediaUpdateMessage:(NSString *)message error:(NSError **)error
+{
+    // Extract the JSON
+    NSData *messageData = [message dataUsingEncoding:NSUTF8StringEncoding];
+    id deserialised = [NSJSONSerialization JSONObjectWithData:messageData
+                                                      options:0
+                                                        error:error];
+    if(*error) {
+        return nil;
+    }
+    
+    // Want to convert the time string to an NSDate
+    static NSDateFormatter *df = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        df = [[NSDateFormatter alloc] init];
+        [df setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSZ"];
+    });
+    
+    // Create a new dictionary with the appropriate values in
+    NSMutableDictionary *parsed = [NSMutableDictionary new];
+    parsed[@"type"] = [deserialised objectForKey:@"type"];
+    parsed[@"time"] = [df dateFromString:[deserialised objectForKey:@"time"]];
+    
+    return [parsed copy];
 }
 
 @end
